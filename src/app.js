498a143758c23bd607f240b23ec5967063004a5d
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2025-10-05 14:03:41 +0530

    exploring get post delete api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 
 const express = require('express');
 const app = express();
+
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Manish",lastName:"Kumar"});
+})
+app.post("/user",(req,res)=>{
+   console.log("Post Request Called");
+   res.send("Data has been posted to the server");
+})
+
+app.delete("/user",(req,res)=>{
+   console.log("Delete Request Called");
+   res.send("Data has been deleted from the server");
+})
+
+// this will match all the routes
 app.use("/",(req,res)=>{
    res.send("Welcome to the home page");
 })
