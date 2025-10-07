8a7eedbe28e313fc34063009a7c25980372d9d59
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2025-10-07 21:51:15 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,44 +1,37 @@
 
 const express = require('express');
+const connectDb = require('./config/database');
 const app = express();
+const { User } = require('./models/user.js');
 
-//app.use("/route", rH, [rH2, rH3], rH4, rh5);
 
-// app.use('/',(err,req,res,next)=>{
-//  if(err){
-//    // Log the error 
-//    res.status(500).send("Something went wrong")
-//  }
-// })
-
-app.use('/getUserData',(req,res,next)=>{
-
-  
-   try{
-      
-  throw new Error("Some error occured")
-   res.send("User data sent")
+app.post('/signup',async(req,res)=>{
+   const userObject = {
+      firstName:'Virat',
+      lastName:'Kohli',
+      emailId:'virat@gmail.com',
+      password:'virat@123',
+      age:45,   
    }
-   catch(err){
-      if(err){
-         res.status(500).send("Some error contact support team")
-      }
-   }
-})
-
-app.use('/',(err,req,res,next)=>{
- if(err){
-   // Log the error 
-   res.status(500).send("Something went wrong")
- }
+   // creating the new instance of the User Model
+   const user = new User(userObject);
+  try{
+   await user.save()
+  res.send('User signed up successfully');
+  }
+  catch(err){
+   res.status(400).send('Error signing up user', err.message);
+  }
 })
 
 
-
-// Handle Auth request for all the request [GET, POST, DELETE, UPDATE]
-
-
-
+connectDb().then(()=>{
+console.log('Connected to database successfully');
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
+}).catch((err)=>{
+    console.log('Error connecting to database', err);
+})
+
+
