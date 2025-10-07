4df2c43a58efd1191722fe61152d63d7b719fc8e
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2025-10-07 17:58:17 +0530

    wild card error handling

--- src/app.js
+++ src/app.js
@@ -1,41 +1,42 @@
 
 const express = require('express');
 const app = express();
-const {adminAuth, userAuth} = require('./middlewares/auth.js')
+
 //app.use("/route", rH, [rH2, rH3], rH4, rh5);
 
+// app.use('/',(err,req,res,next)=>{
+//  if(err){
+//    // Log the error 
+//    res.status(500).send("Something went wrong")
+//  }
+// })
+
+app.use('/getUserData',(req,res,next)=>{
+
+  
+   try{
+      
+  throw new Error("Some error occured")
+   res.send("User data sent")
+   }
+   catch(err){
+      if(err){
+         res.status(500).send("Some error contact support team")
+      }
+   }
+})
+
+app.use('/',(err,req,res,next)=>{
+ if(err){
+   // Log the error 
+   res.status(500).send("Something went wrong")
+ }
+})
 
 
 
 // Handle Auth request for all the request [GET, POST, DELETE, UPDATE]
 
-app.use('/admin',adminAuth)
-
-app.get('/admin/getAllData',(req,res)=>{
-
-   res.send("All Data Sent")
-
-
-
-})
-
-app.delete('/admin/getAllData',(req,res)=>{
-
-   res.send("Data is deleted successfully")
-
-
-
-})
-
-app.post('/user/login',(req,res)=>{
-
-   res.send("User Logged In Successfully")      
-})
-
-app.use('/user',userAuth,(req,res,next)=>{
-   console.log("User Route is being accessed");
-  res.send("User Route Accessed")
-})
 
 
 app.listen(3000,()=>{
