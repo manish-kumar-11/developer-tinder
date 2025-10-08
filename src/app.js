2728f271e32febfcf54b72a26668525d70c0a27a
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2025-10-08 15:53:22 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.post('/signup',async(req,res)=>{
   res.send('User signed up successfully');
   }
   catch(err){
-   res.status(400).send('Error signing up user', err.message);
+   res.status(400).send('Error signing up user' + err.message);
   }
 })
 
@@ -52,6 +52,30 @@ res.send(users)
     }
     catch(err){
         res.status(500).send('Error fetching users', err.message);
+    }
+})
+
+app.delete('/user',async(req,res)=>{
+    const userId = req.body.userId
+    try{
+        const user = await User.findByIdAndDelete(userId)
+       res.send('User deleted successfully');
+
+    }catch(err){
+ res.status(500).send('Error while deleting the  users', err.message);
+    }
+})
+
+// update data of the user
+app.patch('/user',async(req,res)=>{
+    const userId = req.body.userId
+    const data = req.body
+    try{
+     const user = await User.findByIdAndUpdate({_id:userId},data,{new:true,runValidators:true})
+     res.send('User updated successfully');
+    }
+    catch(err){
+        res.status(500).send('Error while updating the  users', err.message);
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,49 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        minLength:2,
+        maxLength:30
     },
     lastName:{
         type:String,
+       
     },
     emailId:{
         type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
     },
     password:{
         type:String,
+        required:true,
     },
     age:{
         type:Number,
+        min:18,
     },
     gender:{
         type:String,
+        validate(value){
+            if(!['male','female','other'].includes(value)){
+                throw new Error("Filled Data(Gender) is not valid")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.pngall.com/wp-content/uploads/5/Profile-PNG-File.png"
+    },
+    about:{
+        type:String,
+        default:"This is default about section"
+    },
+    skills:{
+        type:[String],
     }
-})
+},{timestamps:true})
 
 const User = mongoose.model('User', userSchema);
 module.exports = {User};
